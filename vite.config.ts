e7c83780d326cab6802a91811922cb166edb8a76
Aylong <alexanderkitsa@gmail.com>
2025-07-29 13:04:43 +0300

    Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/SS220-Political-Map/",
   plugins: [react()],
   server: {
     host: "0.0.0.0",
