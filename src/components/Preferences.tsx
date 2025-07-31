f110261568ad192bd7196cb425faab7ca9f46c1f
Aylong <alexanderkitsa@gmail.com>
2025-08-01 00:35:57 +0300

    Allow disable blur

--- src/components/Preferences.tsx
+++ src/components/Preferences.tsx
@@ -1,24 +1,40 @@
 import { useLocalStorage } from "@uidotdev/usehooks";
+import { useEffect } from "react";
 import { Icon, Tooltip } from "tgui-core/components";
 import { classes } from "tgui-core/react";
 
 export function Preferences() {
   const [circlesDisabled, setCirclesDisabled] = useLocalStorage("circles-disabled", false);
   const [spaceBackground, setSpaceBackground] = useLocalStorage("space-background", true);
+  const [backdropBlur, setBackdropBlur] = useLocalStorage("backdrop-blur", true);
+
+  useEffect(() => {
+    if (backdropBlur) {
+      document.documentElement.classList.remove("blur-disabled");
+    } else {
+      document.documentElement.classList.add("blur-disabled");
+    }
+  }, [backdropBlur]);
 
   return (
     <div className="Preferences">
       <PreferenceButton
         icon="circle-o"
-        tooltip={circlesDisabled ? "Включить круги" : "Выключить круги"}
+        tooltip={`${circlesDisabled ? "Включить" : "Выключить"} круги`}
         selected={circlesDisabled}
         onClick={() => setCirclesDisabled(!circlesDisabled)}
       />
       <PreferenceButton
         icon="star"
-        tooltip={spaceBackground ? "Выключить звёзды" : "Включить звёзды"}
+        tooltip={`${spaceBackground ? "Выключить" : "Включить"} звёзды`}
         selected={!spaceBackground}
         onClick={() => setSpaceBackground(!spaceBackground)}
+      />
+      <PreferenceButton
+        icon="droplet"
+        tooltip={`${backdropBlur ? "Выключить" : "Включить"} эффекты размытия`}
+        selected={!backdropBlur}
+        onClick={() => setBackdropBlur(!backdropBlur)}
       />
     </div>
   );
