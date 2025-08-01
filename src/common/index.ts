144471de4c5d464c5031f1acfb05ef68a8c5555a
Aylong <alexanderkitsa@gmail.com>
2025-08-01 10:25:28 +0300

    Auto-deploy

--- src/common/index.ts
+++ src/common/index.ts
@@ -1,4 +1,5 @@
 export { CIRCLE_TEXT_INDENT, maxScale, minScale, POPULATED_SYSTEMS } from "./constants";
 export { findBestLogoPositionAndSize, stringToId, typeToPath } from "./helpers";
 export { Nations } from "./nations";
+export { Planets } from "./planets";
 export { STAR_TYPE, Stars } from "./stars";
